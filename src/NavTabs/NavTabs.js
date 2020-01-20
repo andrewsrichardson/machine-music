4cb8799a743238605b5849b0d7309c84c02ca236
unknown <and.richardson@live.co.uk>
2020-01-20 15:06:49 +0000

    disabled about and train tabs

--- src/NavTabs/NavTabs.js
+++ src/NavTabs/NavTabs.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import { Paper, Tabs, Tab }  from '@material-ui/core';
-
+import React from "react";
+import { Paper, Tabs, Tab } from "@material-ui/core";
 
 export default function NavTabs(props) {
-
   function handleChange(event, newValue) {
     props.updateView(newValue);
   }
 
   return (
     <div>
-        <Paper square>
-          <Tabs
-              value={props.view}
-              indicatorColor="primary"
-              textColor="primary"
-              onChange={handleChange}
-              aria-label="nav tabs"
-              centered="true"
-          >
-              <Tab label="Generate" />
-              <Tab label="Train" />
-              <Tab label="About" />
-          </Tabs>
-        </Paper>
+      <Paper square>
+        <Tabs
+          value={props.view}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={handleChange}
+          aria-label="nav tabs"
+          centered="true"
+        >
+          <Tab label="Generate" />
+          <Tab label="Train" disabled />
+          <Tab label="About" disabled />
+        </Tabs>
+      </Paper>
     </div>
   );
 }
